492a2e541126bbbc84cc93967ea734a389cd553b
heheas <hehe.andrew@gmail.com>
2025-05-06 14:27:22 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -2,16 +2,17 @@ var xPos = 0;
 var yPos = 0;
 var isRunning = false;
 var lastTimestamp = 0;
+var ctx;
 
 $(document).ready(function() {
-   
+   ctx = $('#myCanvas').getContext('2d');
    // Start the game loop
   start();
       
-   /*$('#myCanvas').addEventListener('mousemove', function() {
+   $( "#myCanvas" ).on( "mousemove", function( event ) {
       xPos = evt.pageX - $('#element').offset().left;
       yPos = evt.pageY - $('#element').offset().top;
-   });*/
+   });
 });
 
 // Start the game loop
@@ -46,6 +47,11 @@ function loop(timestamp) {
 
 // Update game state - override this method in your implementation
 function update(deltaTime) {
+   //clear canvas
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   
  // This would be where you update your game state
  console.log(`Frame time: ${deltaTime.toFixed(3)} seconds`);
+  ctx.font = "48px serif";
+  ctx.fillText("Hello world", 10, 50);
 }
