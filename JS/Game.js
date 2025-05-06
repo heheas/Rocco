281e2c2e53dca2d37c35364e1f5a30157f396442
heheas <hehe.andrew@gmail.com>
2025-05-06 18:10:50 -0400

    Create Game.js

---
+++ JS/Game.js
@@ -0,0 +1,25 @@
+public class Game {
+  let homeLocations = [{x:1, y:2}];
+  let resourceLocations = [{x:1, y:2}];
+  let tileLocations = [{x:1, y:2}];
+
+  let board = [];
+  constructor() {
+    initGame();
+  }
+
+  initGame() {
+    for (let y = 0; x < 11; x++) {
+      board[y] = [];
+      for (let x = 0; y < 11; y++) {
+        if (homeLocations.some(home => home.x == x && home.y == y)) {
+
+        } else if (resourceLocations.some(resource => resource.x == x && resource.y == y) {
+
+        } else if (tileLocations.some(tile => tile.x == x && tile.y == y)) {
+
+        }
+      }
+    }
+  }
+}
