5836cedb115cfc8e8dc1d22c66414495a50c7050
heheas <hehe.andrew@gmail.com>
2025-05-06 13:22:39 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -1,3 +1,51 @@
+var xPos = 0;
+var yPos = 0;
+var isRunning = false;
+var lastTimestamp = 0;
+
 $(document).ready(function() {
-   console.log("TEST"); 
+   
+   // Start the game loop
+  start();
+      
+   $('#myCanvas').addEventListener('mousemove', function() {
+      xPos = evt.pageX - $('#element').offset().left;
+      yPos = evt.pageY - $('#element').offset().top;
+   });
 });
+
+// Start the game loop
+start() {
+ if (!this.isRunning) {
+   this.isRunning = true;
+   requestAnimationFrame(this.loop);
+   console.log('Game loop started');
+ }
+}
+
+// Stop the game loop
+stop() {
+ this.isRunning = false;
+ console.log('Game loop stopped');
+}
+
+// The main loop
+loop(timestamp) {
+ // Calculate delta time (time since last frame) in seconds
+ const deltaTime = (timestamp - this.lastTimestamp) / 1000;
+ this.lastTimestamp = timestamp;
+ 
+ if (this.isRunning) {
+   // Update game state
+   this.update(deltaTime);
+   
+   // Request the next frame
+   requestAnimationFrame(this.loop);
+ }
+}
+
+// Update game state - override this method in your implementation
+update(deltaTime) {
+ // This would be where you update your game state
+ console.log(`Frame time: ${deltaTime.toFixed(3)} seconds`);
+}
