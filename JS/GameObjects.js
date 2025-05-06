ece0cd5cca26f9e4699c95b2df02281055754e14
heheas <hehe.andrew@gmail.com>
2025-05-06 19:17:02 -0400

    Update GameObjects.js

--- JS/GameObjects.js
+++ JS/GameObjects.js
@@ -20,6 +20,7 @@ const TileType = {
 class Tile extends GameObject {
   let type = TileType.EMPTY;
   let isDebris = true;
+  let direction = 1; //1-6 directions
   constructor(x, y, type = TileType.EMPTY, isDebris = true) {
     super(x,y);
     this.type = type;
