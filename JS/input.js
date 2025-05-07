a7e0b5cd9d9efb8a188a14b59514185bf8f26e63
heheas <hehe.andrew@gmail.com>
2025-05-07 11:29:38 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -77,7 +77,7 @@ function drawBoard(xPos, yPos, side) {
             } else {
                ctx.fillStyle = "black";
             }
-            let oddfset = y % 2 == 0 ? side/2 : 0;
+            let oddfset = y % 2 == 0 ? 0 : side/2;
             drawHexagon((xPos - side/2 + oddfset) + (x*side*2), (yPos - side/2) + (y*side), side);
          }
      }
