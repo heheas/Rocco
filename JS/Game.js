4f0fddaec7cd21c36b74c87e36cf3b98396cfebb
heheas <hehe.andrew@gmail.com>
2025-05-07 15:33:45 -0400

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -1,6 +1,6 @@
 class Game {
   homeLocations = [{x:3, y:0},{x:0, y:5},{x:5, y:5}, {x:0, y:15},{x:5, y:15},{x:3, y:20}];
-  resourceLocations = [{x:1, y:1},{x:4, y:1},{x:1, y:19},{x:4, y:19},{x:0, y:6},{x:6, y:6}];
+  resourceLocations = [{x:1, y:1},{x:4, y:1},{x:1, y:19},{x:4, y:19},{x:0, y:10},{x:6, y:10}];
   tileLocations = [
     {x:2, y:1},{x:3, y:1},
     {x:2, y:2},{x:3, y:2},{x:4, y:2},
