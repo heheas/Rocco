84172d319d9f7e70dad3ea35a33b26aa6247c3e9
heheas <hehe.andrew@gmail.com>
2025-05-08 08:09:06 -0400

    Update GameObjects.js

--- JS/GameObjects.js
+++ JS/GameObjects.js
@@ -21,10 +21,30 @@ const TileType = {
   EMPTY: "empty"
 }
 
+const HomeType = {
+  ROBOT1: "robot1",
+  ROBOT2: "robot2",
+  ROBOT3: "robot3",
+  ROBOT4: "robot4",
+  ROBOT5: "robot5",
+  ROBOT6: "robot6",
+}
+
+const ResourceType = {
+  CRYSTAL: "crystal",
+  FAUNA: "fauna",
+  MAGNETIC_ORE: "magnetic_ore",
+  NEBULA_CRYSTAL: "nebula_crystal",
+  NUTRIENT_PODS: "nutrient_pods",
+  PLASMA_NODE: "plasma_node"
+}
+
 class Tile extends GameObject {
   type = TileType.EMPTY;
   isDebris = true;
   direction = 1; //1-6 directions
+  homeType = -1; //1-6
+  resourceType = -1; //1-6
 
   constructor(x, y, type = TileType.EMPTY, isDebris = true) {
     super(x,y);
