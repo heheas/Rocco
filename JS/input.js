2f888a41531c12e4c76ff24be959ae125e9b2891
heheas <hehe.andrew@gmail.com>
2025-05-08 08:47:24 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -126,7 +126,7 @@ function drawBackground(x,y, hexSize) {
 
 function loadImages() {
    gameboardIMG = new Image();
-   gameboardIMG.src = "../Art/Gameboard.png";
+   gameboardIMG.src = "./Art/Gameboard.png";
    straightIMG = new Image();
-   straightIMG.src = "../Art/Tiles/Straight.png";
+   straightIMG.src = "./Art/Tiles/Straight.png";
 }
