12d6c0f7055b91b148d6675f00cfa41813b5a7df
heheas <hehe.andrew@gmail.com>
2025-05-11 06:55:25 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -169,7 +169,7 @@ function update(deltaTime) {
    
   ctx.font = Math.floor(16 * scale) + "px serif";
    ctx.fillStyle = "black";
-  ctx.fillText(Math.floor((currentClickX - (gameX - totalBoardWidth/2) - boardHexSize*scale/2)/totalBoardWidth) + ", " + Math.floor((currentClickY - (gameY - totalBoardHeight/2) - boardHexSize*scale/2)/totalBoardHeight), currentClickX, currentClickY);
+  ctx.fillText(Math.floor((currentClickX - (gameX - totalBoardWidth/2) - boardHexSize*scale/2)*boardHexSize/totalBoardWidth) + ", " + Math.floor((currentClickY - (gameY - totalBoardHeight/2) - boardHexSize*scale/2)*boardHexSize/totalBoardHeight), currentClickX, currentClickY);
 }
 
 
