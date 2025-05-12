04f612be4a0168fb76c954dda03c67c290480aec
heheas <hehe.andrew@gmail.com>
2025-05-12 01:52:15 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -87,7 +87,8 @@ function initListeners() {
       currentClickX = event.pageX - $('#myCanvas').offset().left;
       currentClickY = event.pageY - $('#myCanvas').offset().top;
 
-      selectedBoardX = ((currentClickX - (gameX - totalBoardWidth/2) - boardHexSize*scale/2)/totalBoardWidth);
+      //selectedBoardX = ((currentClickX - (gameX - totalBoardWidth/2) - boardHexSize*scale/2)/totalBoardWidth);
+      selectedBoardX = currentClickX;
       selectedBoardY = ((currentClickY - (gameY - totalBoardHeight/2) - boardHexSize*scale/2)/totalBoardHeight);
       //game.selectTile(selectedBoardX, selectBoardY);
    });
