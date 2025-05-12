d38b3032e420fa301353429ff6af2c38a4b8312c
heheas <hehe.andrew@gmail.com>
2025-05-12 14:44:40 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -237,7 +237,7 @@ function update(deltaTime) {
    
    ctx.beginPath();
    ctx.moveTo(xPos, yPos);
-   ctx.lineTo(xPos, yPos + (boardHexSize*(1+spacing)/2*Math.sqrt(3)/2))*scale;
+   ctx.lineTo(xPos, yPos + (scale*boardHexSize*(1+spacing)/2*Math.sqrt(3)/2));
    ctx.closePath();
    ctx.stroke();
 
