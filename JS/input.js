3c02d4148c031b92c17a4404ab7cf4b3a4a20ff5
heheas <hehe.andrew@gmail.com>
2025-05-13 10:46:43 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -105,9 +105,9 @@ function initListeners() {
       var posX = clickX % horizontalSpacing;
       var posY = clickY % verticalSpacing;
 
-      blahX = gameX + (totalBoardWidth/2) + leftX * horizontalSpacing;
+      blahX = gameX - (totalBoardWidth/2) + (leftX * horizontalSpacing);
       blahWidth = horizontalSpacing;
-      blahY = gameY + (totalBoardHeight/2) + topY * verticalSpacing;
+      blahY = gameY - (totalBoardHeight/2) + (topY * verticalSpacing);
       blahHeight = verticalSpacing;
       
       //selectedBoardX = ((currentClickX - (gameX - totalBoardWidth/2) - boardHexSize*scale/2)/totalBoardWidth);
