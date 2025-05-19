1bd91ee1043292597697b1a91204f83004d7f0c2
heheas <hehe.andrew@gmail.com>
2025-05-19 13:08:43 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -260,8 +260,9 @@ function drawBoard(xPos, yPos, hexRadius) {
                   ctx.stroke();
                   
                   ctx.lineDashOffset = 2*selectDashSize-selectDash;
+                  ctx.strokeStyle = "blue";
                   ctx.setLineDash([selectDash, 2*selectDashSize-selectDash]);
-                  drawHexagon(hexX, hexY, radius, null);
+                  drawHexagon(hexX, hexY, radius-5, null);
                   ctx.stroke();
                   
                   ctx.setLineDash([]);
