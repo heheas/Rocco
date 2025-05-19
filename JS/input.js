38f4203c8d9f20eca15eb3fda705d44e09e57983
heheas <hehe.andrew@gmail.com>
2025-05-18 22:09:13 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -252,11 +252,11 @@ function drawBoard(xPos, yPos, hexRadius) {
             if (tile == game.selectedItem) {
                 ctx.strokeStyle = "green";
                if (movingTile) {
-                  ctx.setLineDash([borderOffset, 5-borderOffset]);
+                  ctx.setLineDash([borderOffset, (5-borderOffset)]);
                   drawHexagon(hexX, hexY, radius, null);
                   ctx.stroke();
                   ctx.beginPath();
-                  ctx.setLineDash([5-borderOffSet, borderOffset]);
+                  ctx.setLineDash([(5-borderOffset), borderOffset]);
                }
             } else {
                ctx.strokeStyle = "black";
