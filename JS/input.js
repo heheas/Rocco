46c243ab606dd5e33204c17dc4e3e99ad11e4bd7
heheas <hehe.andrew@gmail.com>
2025-05-20 11:46:55 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -431,7 +431,6 @@ function renderHexagon(x, y, radius, tile) {
             ctx.translate(x, y);
             ctx.rotate((-35+(tile.direction*60))*Math.PI/180);
             ctx.drawImage(robot1IMG,0, 0, 3*radius/4, 3*radius/4);
-            ctx.drawRect(0,0, 10,10);
             ctx.restore();
          }
       }
