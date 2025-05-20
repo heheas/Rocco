b79ae5d3b705cdee28520d0ea31756a7de06fea7
heheas <hehe.andrew@gmail.com>
2025-05-20 09:48:24 -0400

    Update input.js

--- JS/input.js
+++ JS/input.js
@@ -373,13 +373,14 @@ function renderHexagon(x, y, radius, tile) {
             case TileType.HOME:
                ctx.fillStyle = "black";
                ctx.fillStyle = "yellow";
+               /*
                if (tile.HomeType != HomeType.NOTSET) {
                   ctx.save();
                   ctx.translate(x, y);
                   ctx.rotate((-35+(tile.direction*60))*Math.PI/180);
                   ctx.drawImage(robot1IMG,-radius/2, -radius/2, 3*radius/4, 3*radius/4);
                   ctx.restore();
-               }
+               }*/
                break;
             case TileType.RESOURCE:
                ctx.fillStyle = "black";
